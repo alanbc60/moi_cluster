c800f93182f1b43d9cd951df64936c2ab34df5f5
A. G. Medrano-Chavez <ryuuba@gmail.com>
2022-11-23 09:16:03 -0600

    Revert "mpi_conf.sh script works OK"
    This reverts commit ba56aebb36e59b80ecf5bf3fbd9baebf2d24233b.
    
    Test revert.

--- mpi_conf.sh
+++ mpi_conf.sh
@@ -1,55 +1,42 @@
 #!/usr/bin/env bash
 # $1 mode [manager, worker]
-manager_ip=172.30.2.1
-worker1_ip=172.30.2.2
-worker2_ip=172.30.2.3
-worker3_ip=172.30.2.4
-manager_name=rlabred001
-worker1_name=rlabred002
-worker2_name=rlabred003
-worker3_name=rlabred004
+manager=172.30.2.1
+worker1=172.30.2.2
+worker2=172.30.2.3
+worker3=172.30.2.4
 if [ ! -z $1 ];then
     if [ $1 == 'manager' ] || [ $1 == 'worker' ];then
         sudo apt update
-        sudo apt upgrade -y
+        sudo apt upgrade
         sudo apt install sshpass openmpi-bin openmpi-common openmpi-doc libopenmpi-dev python3-pip vim zsh git -y
         echo 'installing mpi4py'
         python -m pip install --upgrade pip
         sudo pip install mpi4py
         echo 'setting ssh'
-        < /dev/zero ssh-keygen -q -t rsa -N '' >/dev/null
+        # ssh-keygen -t rsa # no usar frase
+        yes y | ssh-keygen -q -t rsa -N '' >/dev/null
         cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
+        # eval `ssh-agent`
+        # sshpass -v -p 'raspberry' ssh-add # -P para frase, -p para passwd
         if [ $1 == 'manager' ];then
-            echo 'configuring hosts file'
-            sudo su -c "echo -e '$worker1_ip\t$worker1_name' >> /etc/hosts"
-            sudo su -c "echo -e '$worker2_ip\t$worker2_name' >> /etc/hosts"
-            sudo su -c "echo -e '$worker3_ip\t$worker3_name' >> /etc/hosts"
-            echo 'installing nfs-kernel-server for manager_ip'
+            echo 'installing nfs-kernel-server for manager'
             sudo apt install nfs-kernel-server -y
-            sshpass -v -p 'raspberry' ssh-copy-id -o StrictHostKeyChecking=no pi2@$worker1_ip
-            sshpass -v -p 'raspberry' ssh-copy-id -o StrictHostKeyChecking=no pi2@$worker2_ip
-            sshpass -v -p 'raspberry' ssh-copy-id -o StrictHostKeyChecking=no pi2@$worker3_ip
-            echo 'setting shared directory for manager_ip'
+            sshpass -v -p 'raspberry' ssh-copy-id pi2@$worker1
+            sshpass -v -p 'raspberry' ssh-copy-id pi2@$worker2
+            sshpass -v -p 'raspberry' ssh-copy-id pi2@$worker3
+            echo 'setting shared directory for manager'
             mkdir -p $HOME/mpi-drive
             sudo su -c "echo '$HOME/mpi-drive *(rw,sync,no_root_squash,no_subtree_check)' >> /etc/exports"
             sudo service nfs-kernel-server restart
-            echo 'testing ssh connection'
-            ssh -o StrictHostKeyChecking=no $worker1_name 'echo "hello from $HOST"'
-            ssh -o StrictHostKeyChecking=no $worker2_name 'echo "hello from $HOST"'
-            ssh -o StrictHostKeyChecking=no $worker3_name 'echo "hello from $HOST"'
         elif [ $1 == 'worker' ];then
-            echo 'configuring hosts file'
-            sudo su -c "echo -e '$manager_ip\t$manager_name' >> /etc/hosts"
-            echo 'sending passwd to manager_ip'
-            sshpass -v -p 'raspberry' ssh-copy-id -o StrictHostKeyChecking=no pi2@$manager_ip
+            echo 'sending passwd to manager'
+            sshpass -v -p 'raspberry' ssh-copy-id pi2@$manager
             echo 'installing nfs-common for worker'
             sudo apt install nfs-common -y
             echo 'setting shared directory for worker'
             mkdir -p $HOME/mpi-drive
-            sudo mount -t nfs $manager_ip:/home/pi2/mpi-drive $HOME/mpi-drive
-            sudo su -c "echo '$manager_ip:/home/pi2/mpi-drive $HOME/mpi-drive nfs' >> /etc/fstab"
-            echo 'testing ssh connection'
-            ssh -o StrictHostKeyChecking=no $manager_name 'echo "hello from $HOST"'
+            sudo mount -t nfs $manager:/home/pi2/mpi-drive $HOME/mpi-drive
+            sudo su -c "echo '$manager:/home/pi2/mpi-drive $HOME/mpi-drive nfs' >> /etc/fstab"
         fi
     fi
 else
